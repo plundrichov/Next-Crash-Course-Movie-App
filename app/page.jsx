f5af3094b04160ea52ea88f41d777772acc425c5
plundrichov <miymem@Monikas-MBP.docsis.vodafone.cz>
2023-10-14 15:41:36 +0200

    app folder

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello Next</h1>
+      <h1>hello next</h1>
     </main>
   );
 }
